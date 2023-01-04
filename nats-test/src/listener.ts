af1e78c49f1fe86b61d4d97251a2de5708d634ee
velopace <velopace@outlook.com>
2023-01-03 20:56:50 -0600

    nats streaming test

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,22 @@
+import { randomBytes } from 'crypto';
+import nats, { Message } from 'node-nats-streaming';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe('ticket:created');
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
