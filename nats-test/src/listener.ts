46340a5c0bfa54706f2a024110a36ae03f76775a
velopace <velopace@outlook.com>
2023-01-04 19:59:10 -0600

    manual ack mode and client safe shutdown

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,7 +10,17 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
@@ -18,5 +28,10 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
