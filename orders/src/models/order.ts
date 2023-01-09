37ba13e2729042fd0ddad191ab174ca5c985b9ac
velopace <velopace@outlook.com>
2023-01-09 04:33:21 -0600

    update order schema

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@velopaceticketing/common';
+import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
   userId: string;
---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+interface TicketAttr {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attr: TicketAttr): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttr) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
