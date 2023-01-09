2a92520ea7eb9bba4de561e34908f97a199106d8
velopace <velopace@outlook.com>
2023-01-09 00:31:07 -0600

    validate order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@velopaceticketing/common';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .withMessage('TicketId must be provided')
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
